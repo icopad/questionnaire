0b26d8022b658de2086a319d700a90f9817996c8
Glitch (flapjack-octopus-ws) <none>
2022-01-03 05:15:37 +0000

    😂🌋 Updated with Glitch

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fastify = require("fastify")({
 
 fastify.register(require("fastify-static"), {
   root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
+  prefix: "/"
 });
 
 fastify.register(require("fastify-formbody"));
@@ -20,15 +20,13 @@ fastify.register(require("point-of-view"), {
 const data = require("./src/data.json");
 const db = require("./src/" + data.database);
 
-
 fastify.get("/", async (request, reply) => {
   let params = {};
 
   const options = await db.getChoices();
   if (options) {
-    params.choiceText = options.map(choice => choice.text);
-  }
-  else params.error = data.errorMessage;
+    params.choiceData = options;
+  } else params.error = data.errorMessage;
 
   // Send the page options or raw JSON data if the client requested it
   request.query.raw
@@ -36,40 +34,46 @@ fastify.get("/", async (request, reply) => {
     : reply.view("/src/pages/index.hbs", params);
 });
 
-
-fastify.post("/add", async (request, reply) => {
-  
+fastify.post("/choice", async (request, reply) => {
   let params = {};
-  params.results = true;
-  
+
   let options;
-  
-  if(request.body && request.body.text) {
-    // console.log(request.body.text);
-    params.addResult = await db.addQuestion(request.body.text);
-    console.log(params.addResult);
-    
+
+  if (request.body && request.body.choice) {
+    params.setChoice = await db.setChoice(request.body.choice);
   }
-  // if(request.body.key) {
-  //   console.log(request.body.key)
-  // }
-  console.log(params.failed);
+
+  params.error = params.setChoice ? null : data.errorMessage;
+
   const status = params.failed ? 401 : 200;
   reply.status(status).send(params);
 });
 
+fastify.post("/add", async (request, reply) => {
+  let params = {};
+
+  let options;
+
+  if (request.body && request.body.text) {
+    params.addResult = await db.addQuestion(request.body.text);
+    //console.log(params.addResult);
+  }
+
+  params.error = params.addResult ? null : data.errorMessage;
+
+  const status = params.failed ? 401 : 200;
+  reply.status(status).send(params);
+});
 
 fastify.get("/admin", async (request, reply) => {
-  
   let params = {};
   params.admin = true;
-  
+
   const options = await db.getChoices();
   if (options) {
-    params.choiceText = options.map(choice => choice.text);
-  }
-  else params.error = data.errorMessage;
-  
+    params.choiceData = options;
+  } else params.error = data.errorMessage;
+
   request.query.raw
     ? reply.send(params)
     : reply.view("/src/pages/index.hbs", params);
@@ -77,11 +81,11 @@ fastify.get("/admin", async (request, reply) => {
 
 fastify.post("/reset", async (request, reply) => {
   let params = {};
-  params.deleteResult = await db.clearHistory();
+  params.deleteResult = await db.clearData();
 
   // Check for errors - method would return false value
   params.error = params.deleteResult ? null : data.errorMessage;
-  
+
   const status = params.failed ? 401 : 200;
   reply.status(status).send(params);
 });
